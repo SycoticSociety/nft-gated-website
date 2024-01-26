6783d33a4857dfd2914c088ef809c5e4d560c904
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-01-26 11:41:43 -0500

    Update yourDetails.ts

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -2,7 +2,7 @@
 export const contractAddress = "0x2111ac0D4178274F8f859401a16dB9fBB524986Ep";
 
 // Replace this with your domain name.
-export const domainName = "thirdweb-example.com";
+export const domainName = "https://www.sycoticsociety.com";
 
 // Minimum number of tokens required to access
 export const minimumBalance = 1;
